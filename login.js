347704672fc382aba1b1e051e14bbb92d63753e0
Cyberpunkpranav <pranavsharma733902@gmail.com>
2021-08-13 23:09:08 +0530

    files

---
+++ login.js
@@ -0,0 +1,9 @@
+let notification = document.getElementById("Signup")
+let notify = "you are logging in"
+function SignupEl()
+{
+notify = notify + "."
+    SignupEl.innerText = notification
+}
+console.log(notify)
+
